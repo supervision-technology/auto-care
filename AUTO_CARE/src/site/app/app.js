fc8c8e2af113f4b43e95e01c40c8fe85d13940c2
kavish perera <kavishmanjitha@gmail.com>
2016-10-19 12:22:04 +0530

    app js chnage

--- AUTO_CARE/src/site/app/app.js
+++ AUTO_CARE/src/site/app/app.js
@@ -3,24 +3,12 @@
     angular.module("appModule", [
         "ngRoute",
         "homeModule",
-        "ui.bootstrap",
-        "greenLeavesReceiveModule",
-        "clientAdvanceRequestModule",
-        "clientAdvanceApproveModule",
-        "greenLeavesWeighModule",
-        "greenLeavesPaymentModule",
-        "priceSettingModule",
-        "finalPaymentModule",
-        "monthlyGreenLeavesSummryModule",
-        "routeDetailsModule",
-        "supplierModule"
+        "dailyCheckListModule",
+        "vehicleEntranceModule",
+        "employeeAssignmentModule",
+        "serviceSelectionModule",
+        "bayAssignmentModule"
     ]);
-
-    //constants
-    angular.module("appModule")
-            .constant("systemConfig", {
-                apiUrl: "http://localhost:8080"
-            });
 
     //route config
     angular.module("appModule")
@@ -31,76 +19,33 @@
                             templateUrl: "app/system/home/home.html",
                             controller: "homeController"
                         })
-
-                        //green leaves
-                        .when("/transactions/green-leaves/green-leaves-receive", {
-                            templateUrl: "app/transactions/green-leaves/green-leaves-receive/green-leaves-receive.html",
-                            controller: "greenLeavesReceiveController"
+                        //daily-check-list
+                        .when("/service/daily-check-list", {
+                            templateUrl: "app/service/daily-check-list/daily-check-list.html",
+                            controller: "dailyCheckListController"
                         })
-                        .when("/transactions/green-leaves/client-advance/client-advance-request", {
-                            templateUrl: "app/transactions/green-leaves/client-advance/client-advance-request.html",
-                            controller: "clientAdvanceRequestController"
+                        //employee-assignment
+                        .when("/service/employee-assignment", {
+                            templateUrl: "app/service/employee-assignment/employee-assignment.html",
+                            controller: "employeeAssignmentController"
                         })
-                        .when("/transactions/green-leaves/client-advance/client-advance-approve", {
-                            templateUrl: "app/transactions/green-leaves/client-advance/client-advance-approve.html",
-                            controller: "clientAdvanceApproveController"
+                        //vehicle-entrance
+                        .when("/service/vehicle-entrance", {
+                            templateUrl: "app/service/vehicle-entrance/vehicle-entrance.html",
+                            controller: "vehicleEntranceController"
                         })
-                        .when("/transactions/green-leaves/green-leaves-weigh/green-leaves-weigh", {
-                            templateUrl: "app/transactions/green-leaves/green-leaves-weigh/green-leaves-weigh.html",
-                            controller: "greenLeavesWeighController"
+                        //service-selection
+                        .when("/service/service-selection", {
+                            templateUrl: "app/service/service-selection/service-selection.html",
+                            controller: "serviceSelectionController"
                         })
-                        .when("/transactions/green-leaves/green-leaves-weigh/green-leaves-payment", {
-                            templateUrl: "app/transactions/green-leaves/green-leaves-payment/green-leaves-payment.html",
-                            controller: "greenLeavesPaymentController"
-                        })
-                        .when("/transactions/green-leaves/green-leaves-weigh/price-setting", {
-                            templateUrl: "app/transactions/green-leaves/price-setting/price-setting.html",
-                            controller: "priceSettingController"
-                        })
-                        .when("/transactions/green-leaves/green-leaves-weigh/final-payemnt", {
-                            templateUrl: "app/transactions/green-leaves/final-payemnt/final-payemnt.html",
-                            controller: "finalPaymentModuleController"
-                        })
-                        .when("/transactions/green-leaves/monthly-green-leaves-summry/monthly-green-leaves-summry", {
-                            templateUrl: "app/transactions/green-leaves/monthly-green-leaves-summry/monthly-green-leaves-summry.html",
-                            controller: "monthlyGreenLeavesSummryController"
-                        })
-                        .when("/transactions/green-leaves/route-details", {
-                            templateUrl: "app/transactions/green-leaves/route-details/route-details.html",
-                            controller: "routeDetailsController"
-                        })
-                        .when("/master/supplier/manage-supplier", {
-                            templateUrl: "app/master/supplier/supplier.html",
-                            controller: "routeDetailsController"
+                        //bay-assignment
+                        .when("/service/bay-assignment", {
+                            templateUrl: "app/service/bay-assignment/bay-assignment.html",
+                            controller: "bayAssignmentController"
                         })
                         .otherwise({
                             redirectTo: "/"
                         });
             });
-
-    angular.module("appModule")
-            .config(function (ChartJsProvider) {
-                ChartJsProvider.setOptions({
-                    responsive: true,
-                    maintainAspectRatio: false
-                });
-            });
-
-    angular.module("appModule")
-            .controller("appController", function ($scope, $timeout) {
-                $scope.hamburgerOpen = false;
-
-                $scope.toggleHamburger = function () {
-                    $scope.hamburgerOpen = !$scope.hamburgerOpen;
-
-                    if ($scope.hamburgerOpen) {
-                        $timeout(function () {
-                            angular.element(document.querySelector(".side-bar-left")).css("display", "none");
-                        }, 600);
-                    } else {
-                        angular.element(document.querySelector(".side-bar-left")).css("display", "flex");
-                    }
-                };
-            });
-
 }());
