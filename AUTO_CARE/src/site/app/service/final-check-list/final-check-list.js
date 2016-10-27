3831730e4a7690aa784b3ce81213c6d741263577
kavish perera <kavishmanjitha@gmail.com>
2016-10-27 14:01:04 +0530

    kavish commit

--- AUTO_CARE/src/site/app/service/final-check-list/final-check-list.js
+++ AUTO_CARE/src/site/app/service/final-check-list/final-check-list.js
@@ -9,59 +9,63 @@
                 $scope.vehicle = [
                     {
                         bayName: "Cleaning Bay",
-                        count: "2",
                         image: "/img/05.png",
                         name: "Kasun Chamara",
                         number: "UG 7256"
                     },
                     {
                         bayName: "Washing Bay",
-                        count: "1",
                         image: "/img/01.png",
                         name: "Nidura Prageeth",
                         number: "US 5333"
                     },
                     {
                         bayName: "Vaxing Bay",
-                        count: "0",
                         image: "/img/02.png",
                         name: "Kavish Manjitha",
                         number: "HG 0233"
                     },
                     {
                         bayName: "Vaxing Bay",
-                        count: "0",
                         image: "/img/02.png",
                         name: "Mohan Jayamuni",
-                        number: "HG 0233"
+                        number: "FR 0344"
                     },
                     {
                         bayName: "Vaxing Bay",
-                        count: "0",
                         image: "/img/02.png",
                         name: "Akila Sasanka",
-                        number: "HG 0233"
+                        number: "CAR 8888"
                     },
                 ];
-                $scope.selectedRow = null;
-                $scope.filter = "";
-                $scope.vehicleSelectionDetail = function ($index) {
+                $scope.selectedRow = 0;
+                $scope.isVisible = $scope.isVisible === 0 ? true : false;
+                $scope.filter = "UG 7256";
+                $scope.vehicleSelectionDetail = function ($index,vehicleNum) {
                     $scope.selectedRow = $index;
                     $scope.isVisible = $scope.isVisible === 0 ? true : false;
                     $scope.activePositionVehicle = $scope.activePositionVehicle === $index ? -1 : $index;
+                    $scope.filter= vehicleNum;
+                    
                 };
                 $scope.services = [
                     {
                         indexNo: "S001",
                         name: "Vaxing",
                         check: false,
-                        vehicle: "HG 0233"
+                        vehicle: "UG 7256"
+                    },
+                    {
+                        indexNo: "S0021",
+                        name: "Body Wash",
+                        check: false,
+                        vehicle: "UG 7256"
                     },
                     {
                         indexNo: "S002",
                         name: "Interior Cleaning",
                         check: false,
-                        vehicle: "HG 0233"
+                        vehicle: "US 5333"
                     },
                     {
                         indexNo: "S003",
@@ -70,10 +74,22 @@
                         vehicle: "HG 0233"
                     },
                     {
-                        indexNo: "S004",
-                        name: "Under Wash",
+                        indexNo: "S0033",
+                        name: "Interior Cleaning",
                         check: false,
                         vehicle: "HG 0233"
+                    },
+                    {
+                        indexNo: "S004",
+                        name: "Full Service",
+                        check: false,
+                        vehicle: "FR 0344"
+                    },
+                    {
+                        indexNo: "S004",
+                        name: "Wheel Alignment",
+                        check: false,
+                        vehicle: "CAR 8888"
                     }
                 ];
             });
