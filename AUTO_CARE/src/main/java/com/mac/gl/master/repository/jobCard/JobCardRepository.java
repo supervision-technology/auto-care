6f1c741a81949343882d83939d85699b7d975831
kaza123 <chamara.kaza@gmail.com>
2016-11-24 11:47:15 +0530

    job card

--- AUTO_CARE/src/main/java/com/mac/gl/master/repository/jobCard/JobCardRepository.java
+++ AUTO_CARE/src/main/java/com/mac/gl/master/repository/jobCard/JobCardRepository.java
@@ -6,6 +6,7 @@
 package com.mac.gl.master.repository.jobCard;
 
 import com.mac.gl.master.model.jobCard.JobCard;
+import com.mac.gl.master.model.vehicle.Vehicle;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -16,5 +17,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface JobCardRepository extends JpaRepository<JobCard, Integer>{
 
     public List<JobCard> findByBay(Integer indexNo);
+    public List<JobCard> findTop5ByVehicle(Vehicle vehicle);
     
 }
