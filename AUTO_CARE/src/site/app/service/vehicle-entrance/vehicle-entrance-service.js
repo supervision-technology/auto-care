3237caff694d3e9f60dfbb57b4e5154ce77e675d
Thilina Kalum <kalum323@gmail.com>
2017-03-17 16:34:32 +0530

    Vehicle entrance ,client history ,new client created

--- AUTO_CARE/src/site/app/service/vehicle-entrance/vehicle-entrance-service.js
+++ AUTO_CARE/src/site/app/service/vehicle-entrance/vehicle-entrance-service.js
@@ -1,48 +1,7 @@
-(function () {
-    var service = function ($http, systemConfig) {
-        //master data
-        this.loadVehicle = function () {
-            return $http.get(systemConfig.apiUrl + "/api/care-point/master/vehicle");
-        };
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-        this.loadItems = function () {
-            return $http.get(systemConfig.apiUrl + "/api/care-point/service/item");
-        };
 
-        //pending weight
-        this.getJobHistory = function (vehicle) {
-            return $http.get(systemConfig.apiUrl + "/api/care-point/transaction/job-card/find-job-history/" + vehicle);
-        };
-
-        this.getJobItemHistory = function (jobCard) {
-            return $http.get(systemConfig.apiUrl + "/api/care-point/transaction/job-card/find-job-item-history/" + jobCard);
-        };
-
-        this.saveJobCard = function (data) {
-            return $http.post(systemConfig.apiUrl + "/api/care-point/transaction/job-card/insert-detail", data);
-        };
-
-        //cleint search
-        this.loadClients = function () {
-            return $http.get(systemConfig.apiUrl + "/api/care-point/master/client");
-        };
-        
-        this.loadVehiicleType = function () {
-            return $http.get(systemConfig.apiUrl + "/api/care-point/master/vehicle-type");
-        };
-        
-        this.loadPriceCategory = function () {
-            return $http.get(systemConfig.apiUrl + "/api/care-point/master/priceCategory");
-        };
-        
-        //update vehicle milage
-        this.saveVehicle = function (data) {
-            return $http.post(systemConfig.apiUrl + "/api/care-point/master/vehicle/insert-detail-client-vehicle", data);
-        };
-        
-        
-    };
-
-    angular.module("appModule")
-            .service("VehicleEntranceService", service);
-}());
